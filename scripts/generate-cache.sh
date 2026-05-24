e03f9d6d6f86e058fb138b8524db22abf0a85f0b
Thierry Genovese <tgenovese@vtti.vt.edu>
2026-05-24 18:52:35 -0400

    Update cache

--- scripts/generate-cache.sh
+++ scripts/generate-cache.sh
@@ -8,10 +8,10 @@ cache_dir="${script_dir}/../rosdistro_cache"
 pushd "${cache_dir}/"
 
 # Download upstream cache to make rebuilding way faster
-# for dist in humble jazzy kilted rolling; do
-#     echo "Downloading upstream ${dist}-cache.yaml.gz"
-#     curl -fsSL "http://repo.ros2.org/rosdistro_cache/${dist}-cache.yaml.gz" -o "${dist}-cache.yaml.gz"
-# done
+#for dist in jazzy lyrical rolling; do
+#    echo "Downloading upstream ${dist}-cache.yaml.gz"
+#    curl -fsSL "http://repo.ros2.org/rosdistro_cache/${dist}-cache.yaml.gz" -o "${dist}-cache.yaml.gz"
+#done
 
 # Rebuild cache files, adding our packages
 rosdistro_build_cache "${script_dir}/../index-v4.yaml"
